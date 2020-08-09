d33000860fca3a8b90b779913ce8a2cd0f28a74f
taigaprog <taigaprog@gmail.com>
2020-08-09 17:30:02 +0900

    Initial Commit

---
+++ SwiftSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftSample
+//
+//  Created by Taiga Ozawa on 2020/08/09.
+//  Copyright © 2020 Taiga Ozawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
